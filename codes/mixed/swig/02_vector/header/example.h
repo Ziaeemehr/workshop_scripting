c7c693f3b578666f94a273b561e03b1b995bef8e
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2018-12-28 19:45:40 +0330

    swig example modified

---
+++ codes/mixed/swig/02_vector/header/example.h
@@ -0,0 +1,8 @@
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+
+double average(std::vector<int> v);
+std::vector<double> half(const std::vector<double> &v);
+void halve_in_place(std::vector<double> &v);
