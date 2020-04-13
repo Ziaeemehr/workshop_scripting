55e7b8131ad7a650d3347cd261a523d664209f2e
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2020-04-13 15:22:51 +0430

    3dplot demoes added.

---
+++ codes/05plot/3dplot/wire3d_demo.py
@@ -0,0 +1,23 @@
+'''
+=================
+3D wireframe plot
+=================
+
+A very basic demonstration of a wireframe plot.
+'''
+
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+import sys
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Grab some test data.
+X, Y, Z = axes3d.get_test_data(0.05)
+
+# Plot a basic wireframe.
+ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
+plt.savefig(sys.argv[0].split(".")[0] + ".png")
+
+# plt.show()
