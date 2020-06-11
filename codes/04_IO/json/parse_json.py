62ef6b782ff3b5de7e2baf67b6a7049aa8510a49
Abolfazl Ziaeemehr <a.ziaeemehr@gmail.com>
2020-06-11 11:04:21 +0430

    parse json

--- codes/04_IO/json/parse_json.py
+++ codes/04_IO/json/parse_json.py
@@ -4,7 +4,7 @@ import json
 
 json_data = '{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5}'
 parsed_json = (json.loads(json_data))
-print (json.dumps(parsed_json, indent=4, sort_keys=True))
+print(json.dumps(parsed_json, indent=4, sort_keys=True))
 
 
 with open("person.json") as f:
@@ -14,21 +14,21 @@ print(loaded_json)
 print(json.dumps(loaded_json, indent=4))
 
 for x in loaded_json:
-    print (x)
+    print(x)
 
 
 # Writing JSON to a file
 
 person1 = {"name": "Bob",
-               "languages": ["English", "Fench"],
-               "married": True,
-               "age": 32
-               }
+           "languages": ["English", "Fench"],
+           "married": True,
+           "age": 32
+           }
 person2 = {"name": "Ali",
-               "languages": ["English", "Farsi"],
-               "married": True,
-               "age": 33
-               }
+           "languages": ["English", "Farsi"],
+           "married": True,
+           "age": 33
+           }
 list_of_dict = [person1, person2]
 
 with open('person_w.json', 'w') as json_file:
